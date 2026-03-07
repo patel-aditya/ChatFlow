ff6e41741f71ceee281f7909f29daa7df419d9f5
Aditya Patel <adityapatelatwork@gmail.com>
2026-03-08 04:49:40 +0530

    done security and authrization

---
+++ backend/core/auth.py
@@ -0,0 +1,54 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+
+from config import settings
+from schemas.user import TokenData
+from models.user import User
+from database import get_db
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def create_access_token(data:dict) -> str:
+    to_encode = data.copy()
+    expire = datetime(timezone.utc()) + timedelta(minutes =settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, settings.SECRET_KEY,algorithm=settings.ALGORITHM)
+
+    return token
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate":"Bearer"},
+        )
+
+def get_current_user(token: str = Depends(oauth2_scheme), db:Session = Depends(get_db)) -> User:
+    payload = decode_access_token(token)
+    user_id: str = payload.get("sub")
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate":"Bearer"},
+        )
+    
+    token_data = TokenData(user_id=int(user_id))
+    user = db.query(User).filter(User.id == token_data).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User Not found",
+            headers={"WWW-Authenticate":"Bearer"},
+        )
+    return user
---
+++ backend/core/connection_manager.py
@@ -0,0 +1,28 @@
+from fastapi import WebSocket
+from typing import List, Dict
+
+class ConnectionManager:
+    def __int__(self):
+        self.active_connections: Dict[int, WebSocket] = {}
+
+    async def connect(self, user_id: int, websocket:WebSocket):
+        await websocket.accept()
+        self.active_connections[user_id] = websocket
+
+    def disconnect(self, user_id: int):
+        self.active_connections.pop(user_id, None)
+    
+    async def send_personal_message(self, message: dict, user_id: int):
+        websocket = self.active_connections.get(user_id)
+        if websocket:
+            await websocket.send_json(message)
+
+    async def broadcast_to_users(self, message: dict, user_ids: List[int]);
+        for user_id in user_ids:
+            await self.send_personal_message(message, user_id)
+
+    def is_online(self, user_id: int) ->bool;
+        return user_id in self.active_connections
+    
+    
+manager = ConnectionManager()
